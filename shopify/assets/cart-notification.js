bc080f5feccbda5c641f9e0c0434dcac787af361
ngohoanglong <ngohoanglongptit@gmail.com>
2021-11-30 18:08:17 +0700

    update cart

--- shopify/assets/cart-notification.js
+++ shopify/assets/cart-notification.js
@@ -2,72 +2,83 @@ class CartNotification extends HTMLElement {
   constructor() {
     super();
 
-    this.notification = document.getElementById('cart-notification');
-    this.header = document.querySelector('sticky-header');
+    this.notification = document.getElementById("cart-notification");
+    this.header = document.querySelector("sticky-header");
     this.onBodyClick = this.handleBodyClick.bind(this);
-    
-    this.notification.addEventListener('keyup', (evt) => evt.code === 'Escape' && this.close());
+
+    this.notification.addEventListener(
+      "keyup",
+      (evt) => evt.code === "Escape" && this.close()
+    );
     this.querySelectorAll('button[type="button"]').forEach((closeButton) =>
-      closeButton.addEventListener('click', this.close.bind(this))
+      closeButton.addEventListener("click", this.close.bind(this))
     );
   }
 
   open() {
-    this.notification.classList.add('animate', 'active');
+    this.notification.classList.add("animate", "active");
 
-    this.notification.addEventListener('transitionend', () => {
-      this.notification.focus();
-      trapFocus(this.notification);
-    }, { once: true });
+    this.notification.addEventListener(
+      "transitionend",
+      () => {
+        this.notification.focus();
+        trapFocus(this.notification);
+      },
+      { once: true }
+    );
 
-    document.body.addEventListener('click', this.onBodyClick);
+    document.body.addEventListener("click", this.onBodyClick);
   }
 
   close() {
-    this.notification.classList.remove('active');
+    this.notification.classList.remove("active");
 
-    document.body.removeEventListener('click', this.onBodyClick);
+    document.body.removeEventListener("click", this.onBodyClick);
 
     removeTrapFocus(this.activeElement);
   }
 
   renderContents(parsedState) {
-      this.productId = parsedState.id;
-      this.getSectionsToRender().forEach((section => {
-        document.getElementById(section.id).innerHTML =
-          this.getSectionInnerHTML(parsedState.sections[section.id], section.selector);
-      }));
+    this.productId = parsedState.id;
+    this.getSectionsToRender().forEach((section) => {
+      document.getElementById(section.id).innerHTML = this.getSectionInnerHTML(
+        parsedState.sections[section.id],
+        section.selector
+      );
+    });
 
-      this.header?.reveal();
-      this.open();
+    this.header?.reveal();
+    this.open();
   }
 
   getSectionsToRender() {
     return [
       {
-        id: 'cart-notification-product',
-        selector: `#cart-notification-product-${this.productId}`,
+        id: "cart-notification-product",
+        selector: `#cart-notification`,
       },
       {
-        id: 'cart-notification-button'
+        id: "cart-notification-button",
       },
       {
-        id: 'cart-icon-bubble'
-      }
+        id: "cart-icon-bubble",
+      },
     ];
   }
 
-  getSectionInnerHTML(html, selector = '.shopify-section') {
+  getSectionInnerHTML(html, selector = ".shopify-section") {
     return new DOMParser()
-      .parseFromString(html, 'text/html')
+      .parseFromString(html, "text/html")
       .querySelector(selector).innerHTML;
   }
 
   handleBodyClick(evt) {
     const target = evt.target;
-    if (target !== this.notification && !target.closest('cart-notification')) {
-      const disclosure = target.closest('details-disclosure');
-      this.activeElement = disclosure ? disclosure.querySelector('summary') : null;
+    if (target !== this.notification && !target.closest("cart-notification")) {
+      const disclosure = target.closest("details-disclosure");
+      this.activeElement = disclosure
+        ? disclosure.querySelector("summary")
+        : null;
       this.close();
     }
   }
@@ -77,4 +88,192 @@ class CartNotification extends HTMLElement {
   }
 }
 
-customElements.define('cart-notification', CartNotification);
+customElements.define("cart-notification", CartNotification);
+
+class NotificationCartRemoveButton extends HTMLElement {
+  constructor() {
+    super();
+    this.addEventListener("click", (event) => {
+      event.preventDefault();
+      this.closest("notification-cart-items").updateQuantity(this.dataset.index, 0);
+    });
+  }
+}
+
+customElements.define("notification-cart-remove-button", NotificationCartRemoveButton);
+
+class NotificationCartItems extends HTMLElement {
+  constructor() {
+    super();
+
+    this.lineItemStatusElement = document.getElementById(
+      "shopping-cart-line-item-status"
+    );
+
+    this.currentItemCount = Array.from(
+      this.querySelectorAll('[name="updates[]"]')
+    ).reduce(
+      (total, quantityInput) => total + parseInt(quantityInput.value),
+      0
+    );
+
+    this.debouncedOnChange = debounce((event) => {
+      this.onChange(event);
+    }, 300);
+
+    this.addEventListener("change", this.debouncedOnChange.bind(this));
+  }
+
+  onChange(event) {
+    this.updateQuantity(
+      event.target.dataset.index,
+      event.target.value,
+      document.activeElement.getAttribute("name")
+    );
+  }
+
+  getSectionsToRender() {
+    return [
+      {
+        id: "cart-notification-product",
+        section: "cart-notification-product",
+        selector: `#cart-notification`,
+      },
+      {
+        id: "cart-icon-bubble",
+        section: "cart-icon-bubble",
+        selector: ".shopify-section",
+      },
+      {
+        id: "cart-live-region-text",
+        section: "cart-live-region-text",
+        selector: ".shopify-section",
+      },
+    ];
+  }
+
+  updateQuantity(line, quantity, name) {
+    this.enableLoading(line);
+
+    const body = JSON.stringify({
+      line,
+      quantity,
+      sections: this.getSectionsToRender().map((section) => section.section),
+      sections_url: window.location.pathname,
+    });
+
+    fetch(`${routes.cart_change_url}`, { ...fetchConfig(), ...{ body } })
+      .then((response) => {
+        return response.text();
+      })
+      .then((state) => {
+        const parsedState = JSON.parse(state);
+        this.classList.toggle("is-empty", parsedState.item_count === 0);
+        document
+          .getElementById("main-cart-footer")
+          ?.classList.toggle("is-empty", parsedState.item_count === 0);
+
+        this.getSectionsToRender().forEach((section) => {
+          console.log({parsedState,section,'parsedState.sections[section.section]':parsedState.sections[section.section]})
+          const elementToReplace =
+            document
+              .getElementById(section.id)
+              .querySelector(section.selector) ||
+            document.getElementById(section.id);
+
+          elementToReplace.innerHTML = this.getSectionInnerHTML(
+            parsedState.sections[section.section],
+            section.selector
+          );
+        });
+
+        this.updateLiveRegions(line, parsedState.item_count);
+        document
+          .getElementById(`CartItem-${line}`)
+          ?.querySelector(`[name="${name}"]`)
+          ?.focus();
+        this.disableLoading();
+      })
+      .catch((error) => {
+        console.error(error)
+        this.querySelectorAll(".loading-overlay").forEach((overlay) =>
+          overlay.classList.add("hidden")
+        );
+        document.getElementById("cart-errors").textContent =
+          window.cartStrings.error;
+        this.disableLoading();
+      });
+  }
+
+  updateLiveRegions(line, itemCount) {
+    if (this.currentItemCount === itemCount) {
+      // document
+      //   .getElementById(`Line-item-error-${line}`)
+      //   .querySelector(".cart-item__error-text").innerHTML =
+      //   window.cartStrings.quantityError.replace(
+      //     "[quantity]",
+      //     document.getElementById(`Quantity-${line}`).value
+      //   );
+    }
+
+    this.currentItemCount = itemCount;
+    this.lineItemStatusElement.setAttribute("aria-hidden", true);
+
+    const cartStatus = document.getElementById("cart-live-region-text");
+    cartStatus.setAttribute("aria-hidden", false);
+
+    setTimeout(() => {
+      cartStatus.setAttribute("aria-hidden", true);
+    }, 1000);
+  }
+
+  getSectionInnerHTML(html, selector) {
+    console.log({
+      html,selector
+    })
+    return new DOMParser()
+      .parseFromString(html, "text/html")
+      .querySelector(selector).innerHTML;
+  }
+
+  enableLoading(line) {
+    document
+      .getElementById("main-cart-items")
+      .classList.add("cart__items--disabled");
+    this.querySelectorAll(".loading-overlay")[line - 1].classList.remove(
+      "hidden"
+    );
+    document.activeElement.blur();
+    // this.lineItemStatusElement.setAttribute("aria-hidden", false);
+  }
+
+  disableLoading() {
+    document
+      .getElementById("main-cart-items")
+      .classList.remove("cart__items--disabled");
+  }
+}
+
+customElements.define("notification-cart-items", NotificationCartItems);
+
+class NotificationCartNote extends HTMLElement {
+  constructor() {
+      super();
+
+      this.addEventListener('change', debounce((event) => {
+          const body = JSON.stringify({
+              note: event.target.value
+          });
+          fetch(`${
+              routes.cart_update_url
+          }`, {
+              ...fetchConfig(),
+              ...{
+                  body
+              }
+          });
+      }, 300))
+  }
+}
+
+customElements.define('notification-cart-note', NotificationCartNote);
