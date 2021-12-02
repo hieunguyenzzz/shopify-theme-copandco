e11a72df758e178bd8321a65c0a103898708836f
ngohoanglong <ngohoanglongptit@gmail.com>
2021-12-02 17:28:59 +0700

    update

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -15,10 +15,9 @@ module.exports = {
         maxWidth:[],
         padding: {
           DEFAULT: '1rem',
-          sm: '2rem',
-          lg: '4rem',
-          xl: '5rem',
-          '2xl': '5rem',
+          sm: '1rem',
+          lg: '1rem',
+          xl: '2.5rem',
         },
       },
       fontFamily:{
